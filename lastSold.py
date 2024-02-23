8e8c78f3f53f5e81c066c4590ce157497ba6de46
Christian Carbeau <ctc53@duke.edu>
2024-02-23 17:57:37 -0500

    finished price finding algorithm

--- lastSold.py
+++ lastSold.py
@@ -3,34 +3,76 @@ import requests
 import pandas as pd
 import numpy as np
 
-def soldPrice():
-    # NEED TO FIX LAST SOLD DATE INPUT
-    item = "2023-topps-chrome-anthony-volpe"
-    count = 10
-    date = "[2024-01-30T00:00:00Z..2024-02-17T00:00:00Z]"
-    token = "v^1.1#i^1#f^0#r^0#I^3#p^1#t^H4sIAAAAAAAAAOVYa2gUVxTO5lW20QQf2BICXUeFatjZO6/s7uiubhJDVmMSs2nU0CJ3Zu4kY3ZnJnPvmg1V2KatSilILFWLFe0DtFTFH1KJ1VoCbaFo2wilUNrSH/1hsUiNbS22QmcmMW5S8ZWFBrp/ljn33HO/77vn3HNnQLbUu2xH444bsz2PFR7Ogmyhx8OUAW9pSXV5UWFlSQHIcfAczi7OFg8UXV6BYSppim0Im4aOkS+TSupYdI0RKm3pogGxhkUdphAWiSwmYuuaRJYGomkZxJCNJOWL10coTmXZUI2icDIjsDWSZFv12zHbjQglSQrgpaDEAFkOsWHGHsc4jeI6JlAnEYoFLO8HrJ/l2pmQyAsiD+hgiO2kfB3Iwpqh2y40oKIuXNGda+VgvTdUiDGyiB2EisZjDYmWWLx+dXP7ikBOrOi4DgkCSRpPfqozFOTrgMk0uvcy2PUWE2lZRhhTgejYCpODirHbYB4Bviu1JKuMCgSek7kgh0JcXqRsMKwUJPfG4Vg0xa+6riLSiUb676eorYa0Bclk/KnZDhGv9zl/69MwqakasiLU6trYplhrKxWt67Y0TDToxwhacre/ta3eL0EEBYFlBL/A17C8xHDjy4zFGhd5yjp1hq5ojmTY12yQWmRjRlOV4XKUsZ1a9BYrphIHT44fC24rGAx3Ols6todp0q07u4pStgw+9/H++k/MJsTSpDRBExGmDrgCRShomppCTR10M3E8eTI4QnUTYoqBQF9fH93H0YbVFWABYAIb1zUl5G6UgpTr69S646/df4Jfc6nIyJ6JNZH0mzaWjJ2pNgC9i4ryoXA4DMZ1nwwrOtX6L0MO58DkeshXfQQhCMlAgILMh2EwKOSjPqLjKRpwcCAJ9vtT0OpBxExCGfllO8/SKWRpip1UKsuFVORXasKqnw+rql8SlBo/oyIEEJIkORz6/5TJgyZ6AskWInnK9Dxlebh3aybGENWqDaodoXa7l3VWG4nQlrretrY1tS0oYEG+WRbYDb1dkQethbuSr0tqtjLt9vr5EsCp9fyI0GhggpRp0UvIholajaQm98+sDeYspRVapD+BkknbMC2SMdOM5+ukzhO9hzokHo11PvvTf9Kb7soKOwk7s1g587EdAJoa7XQfWjZSAQPa146AU+u2ebOLelq8NfvOOqNY2yTH2GrK2GWTdinTeKtMWwgbacu+Z9Mtzu2r3ehBut3NiGUkk8jqYKZdzalUmkApiWZaWechwTU4w1otEwShIMOF+enxkt1GunmmHUn5O4iLVz7khTow+eU+WuD+mAHPMBjwfFTo8YAVYAmzCCwsLXqmuGhWJdYIojWo0ljr0u13VgvRPajfhJpVOK9g9O3XG+sqV7fsXfZ8e/9XBz4rmJXzbeHwc+DJia8L3iKmLOdTA6i6M1LCVDwxm+UBy3JMiBd40AkW3RktZhYUzz81IPx1cGlt1Q8/Xt3w6Z9HB6t6tnaC2RNOHk9JQfGAp0DqHL14s6zk3bWZ3kPXRy81VJ9+def3v4+81Xw8dWZO+RL5b29mJLNxd8VLRz7Zd/HL6jcPvFe3yXttePnXxz43y4dOPv5xJHGeWtx86rX43Mvf7hR/WVh5Zv/8VdTgCSXe0J2qLN/4imfVhdPbjm+rOLLg/RtzSt8ZGdr+7CH51rzdzTd/unLTS323J2Ce+6bq6VtrF7dere04JWX3LOee+jCxa822F07uOnhm4bDZVj0QiQ1eTJ6tX/JF7QnhZ++xpuj2+kObmzYouE/K/mbNu7L+156Xz41cOaEPnx8KDf3xYlVZ+VJw7foH5Sx9qfHGXN+Rwa4FJZTKVKyEb+y/sFfIHA2cBefWlY6O7eU/Fm54oPURAAA="
-    url = "https://api.ebay.com/buy/browse/v1/item_summary/search?q="+item+"&limit="+str(count)+"&filter=buyingOptions:{FIXED_PRICE}&sort=price"
+# Method that returns the lowest buy now prices for an "item" in a dataFrame that includes the items title, price, and eBay link
+def soldPrice(item):
+
+    # How many items to search (200 is max)
+    count = 200
+
+    # 2750 is the condition id for a graded card 
+    condition = "2750"
+
+    # Zip code that the item can ship to
+    zipCode = "07960"
+
+    # Date that you want the cards to end before 
+    # Must use UTC ISO 8601 format of [yyyy-MM-ddThh:mm:ss.sssZ]
+    # .. in front of yyyy means from now up to that date
+    # date = "[..2024-02-24T18:00:000Z]"
+
+    # Permission code from ebay 
+    token = "v^1.1#i^1#f^0#I^3#p^1#r^0#t^H4sIAAAAAAAAAOVYa2wUVRTe3W5LsC0QkUcqwTo8jCUzO6/t7ky6C9supMujr11rKVK8O3OnnXZ3Zph7l3bBmFoISkIgmuALYhsSf2gUNVGIjxgCGhEENDE+EmPQECUSiRAN8gNwZrqUbSW8uolN3D+bOffcc7/vu+fcc2fo/pLJVVvrt14sd0/yDPXT/R63mymlJ5cUL5pS5KkodtF5Du6h/vn93oGiMzUIpFOG2AKRoWsIVvalUxoSHWOIyJiaqAOkIlEDaYhELInxyKqVIkvRomHqWJf0FFEZi4YIhWMZGgRkDtBBhpYFy6pdi5nQQwTkYLUElKTAy3yQlhRrHKEMjGkIAw2HCJZmeZJmSZZLsIzIB0WeoViWbycqW6GJVF2zXCiaCDtwRWeumYf15lABQtDEVhAiHIssizdGYtGlDYkaX16scE6HOAY4g0Y/1ekyrGwFqQy8+TLI8RbjGUmCCBG+8PAKo4OKkWtg7gK+I3WS8XNcAIDqoCL4/UKgIFIu0800wDfHYVtUmVQcVxFqWMXZWylqqZHshhLOPTVYIWLRSvuvOQNSqqJCM0QsrY2sjjQ1EeG6LlNFWAUkgsCUusimliiZBBD4/SzjJ/18NcsnGS63zHCsnMhj1qnTNVm1JUOVDTquhRZmOFYZLk8Zy6lRazQjCrbx5PtxIwoy7faWDu9hBndp9q7CtCVDpfN4a/1HZmNsqskMhiMRxg44AoUIYBiqTIwddDIxlzx9KER0YWyIPl9vby/Vy1G62eljaZrxta1aGZe6YBoQjq9d67a/eusJpOpQkaA1E6kizhoWlj4rUy0AWicR5oOCINA53UfDCo+1/suQx9k3uh4KVR98tSAxAVYJsBxgASjIURPOpajPxgGTIEumgdkDsZECEiQlK88yaWiqssj5FZYLKpCUqwWF5AVFIZN+uZpkFAhpCJNJSQj+f8rkdhM9DiUT4gJleoGyXFi/oS/CYMWsDSitwYTVy9oX6fFgd936lpbltY3QZwK+QfKzj67vDN1uLdyQfF1KtZRJWOsXSgC71gsjQr2OMJTHRS8u6QZs0lOqlJ1YG8yZchMwcTYOUynLMC6SEcOIFeqkLhC9Ozok7o51IfvTf9KbbsgK2Qk7sVjZ85EVABgqZXcfStLTPh1Y1w6fXeuWeZ2Dely8VevOOqFYWySH2ary8GWTcihTaINEmRDpGdO6Z1ON9u0rofdAzepm2NRTKWi2MuOu5nQ6g0EyBSdaWRcgwVUwwVotE6CDAZYX2PFtm+Q00nUT7Ugq3EHsXXyHF2rf6Jf7sMv5MQPuQ/SA+2OP203X0AuYefSDJUWPeIvKKpCKIaUChUJqp2a9s5qQ6oFZA6imZ7rrwt5d9XUVSxufr9qUyH65+zNXWd63haG19OyRrwuTi5jSvE8N9JzrI8XM1FnlLE+zLMcyfJBn2ul510e9zEzvfXTz8q+fWCCc3H6psfejBYdnJVd89SZdPuLkdhe7vANul/Ze+a6yCmHKldqOp/VvQud2UFs6zCk1sKTqyYvGrKs+Lbrn84rpqeMrpr1yfPZjC9/pKu3wEDVr3//pVIirGdr/59nX5S+OLNTY7zZeuHpQ3DjY3LbquZ+XfLr5qUDvs614y7ZTz7y7d9eLbTUfnjw8d8Oa+NDpPb92D7r+eujtPyadm/7J7upBc87Llw8cvfjSvcdAzwNDC8/+cmbq+UPMkswWNHPjjuZtxuZjcz0o9laTsM831bukQ0pED52/tO+30AVyTf20Vxu+v3/7SfzDt4tOtK3+AEbr5b+7r9ZvmhE5ff73g48fWH3P0cGyCrpUuvxG4Mri+a9deeHhFXvmHDndsvPHGVXxEzPg/vjwXv4D4LzwVPURAAA="
+    
+    # API URL
+    url = "https://api.ebay.com/buy/browse/v1/item_summary/search?q="+item+"&limit="+str(count)+"&filter=buyingOptions:{FIXED_PRICE}&sort=price&filter=conditionIds"+condition+"&filter=deliveryPostalCode:"+zipCode
     headers = {"Authorization":"Bearer "+token,
                 "X-EBAY-C-MARKETPLACE-ID":"EBAY_US",
-                "X-EBAY-C-ENDUSERCTX":"affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>",
-                "X-EBAY-C-ENDUSERCTX": "contextualLocation=country%3DUS%2Czip%3D19406"}
+                "X-EBAY-C-ENDUSERCTX":"affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>"}
+    
+    # Make API Get request 
     apiReq = requests.get(url,headers=headers)
+
+    # Parse json file
     parseddoc = apiReq.json()
-    print(parseddoc)
+
+    # Create series's for each items' title, price, and link
     prices = pd.Series(index = np.arange(count))
     ids = [None]*count
+    links = [None]*count
+
+    # Traverse the JSON document and extract desired data
     i = 0
     for item in (parseddoc["itemSummaries"]):
-        title = item["title"]
-        ids[i]=title
-        # condition = item["condition"][0]["conditionDisplayName"][0]
-        print(item)
-        price = item["price"]["value"]
+        try: 
+            ids[i]=item["title"]
+        except: 
+            ids[i]="None"
+
+        try:
+            links[i]=item["itemWebUrl"]
+        except:
+            links[i]="None"
+        
+        try:
+            price = float(item["price"]["value"])
+        except:
+            price = 100000
+        try:
+            price = price + float(item["shippingOptions"][0]["shippingCost"]["value"])
+        except:
+            # Add an arbitrary 5 dollars if shipping price isn't specified 
+            price = price + 5
+
         prices[i] = float(price)
         i = i + 1
-    ret = pd.Series(index = ids, data = prices.values)
-    print(ret)
-    # return ret
 
-if __name__ == "__main__":
-    soldPrice()
+    # Create dataFrame with the series created in the loop above
+    data = pd.DataFrame(columns = ["Title", "Price", "Link"])
+    data["Title"]=ids
+    data["Price"]=prices
+    data["Link"]=links
+
+    # Sort dataFrame by price from least to greatest
+    data = data.sort_values(by="Price")
+
+    return data
