2d245acdd0cf38e4bc201644b0439dcf13bf00d2
Christian Carbeau <ctc53@duke.edu>
2024-02-23 14:41:59 -0500

    facing sort problems

--- lastSold.py
+++ lastSold.py
@@ -5,27 +5,31 @@ import numpy as np
 
 def soldPrice():
     # NEED TO FIX LAST SOLD DATE INPUT
-    item = "anthony-volpe"
+    item = "2023-topps-chrome-anthony-volpe"
     count = 10
     date = "[2024-01-30T00:00:00Z..2024-02-17T00:00:00Z]"
-    url = "https://api.ebay.com/buy/browse/v1/item_summary/search?q="+item+"&limit="+str(count)+"&filter=buyingOptions:{AUCTION|FIXED_PRICE}&filter=lastSoldDate:[2024-01-30T00:00:00Z..2024-02-17T00:00:00Z]"
-    headers = {"Authorization":"Bearer v^1.1#i^1#r^0#I^3#f^0#p^1#t^H4sIAAAAAAAAAOVYfWwURRTvXXuYSvkwgEADemwlJCW7N7t3e71deyfXr7QV2tI7SltFnN2dbZfe7V5352hPg5aiICQYiFAhSiQohmAM8QuJMWAQqBiMqAQTNIpYlH9AMSaEgMbdbSnXSvjqJTbx/rnsmzdvfr/fvDdvdkHXmNzC1ZWrL41z3OPc3gW6nA4HPRbkjnHNHZ/tzHdlgTQHx/auh7pyurPPFRswHkvw9chIaKqB3J3xmGrwtjFIJHWV16ChGLwK48jgschHwgvm8wwF+ISuYU3UYoS7qixIcELAxwX8fi8t+2mGYU2rei1mVAsSghwQkFjE+f0+WhIkrzluGElUpRoYqjhIMIDxkYAhaS5Ke3mW42lA+dlAM+FuQLqhaKrpQgEiZMPl7bl6GtabQ4WGgXRsBiFCVeGKSG24qqy8JlrsSYsVGtAhgiFOGkOfSjUJuRtgLIluvoxhe/ORpCgiwyA8of4Vhgblw9fA3AV8W2qv4GckLwD+Ip/EyhKXESkrND0O8c1xWBZFImXblUcqVnDqVoqaagjLkIgHnmrMEFVlbutvYRLGFFlBepAoLwk3hevqiFBpq64YWIGkgaAutpJ19WWkABFkWYZmSdbnZ3wC7R1Ypj/WgMjD1inVVEmxJDPcNRouQSZmNFwZX5oyplOtWquHZWzhSfdjBxVkm60t7d/DJG5VrV1FcVMGt/14a/0HZ2OsK0ISo8EIwwdsgYIETCQUiRg+aGfiQPJ0GkGiFeME7/F0dHRQHV5K01s8DAC0p3HB/IjYiuKQsH2tWrf8lVtPIBWbiojMmYbC41TCxNJpZqoJQG0hQr4Ax3FgQPehsELDrf8ypHH2DK2HTNUHJ9CSTAPGX8QFfILfl4n6CA2kqMfCgQSYIuNQb0M4EYMiIkUzz5JxpCsS72VlxhuQESn5OZn0cbJMCqzkJ2kZIYCQIIhc4P9TJreb6BEk6ghnKNMzlOVc+/LOMI1lvaRIbghEzV7WPFeLBJaVttfXV5fUIo8OfTUiyyxubwnebi3ckHxpTDGViZrrZ0oAq9YzI0KlZmAkjYheRNQSqE6LKWJqdG2wV5fqoI5TERSLmYYRkQwnElWZOqkzRO+ODom7Y53J/vSf9KYbsjKshB1drKz5hhkAJhTK6j6UqMU9GjSvHR6r1k3zUhv1iHgr5p11VLE2SfazVaT+yyZlU6aM5SKlI0NL6uY9m6q1bl9RrQ2pZjfDuhaLIb2BHnE1x+NJDIUYGm1lnYEEV+Aoa7V0EQh4WZpmR8ZLtBvp0tF2JGXuIM555A4v1J6hL/ehLPtHdzsOgm7HfqfDAYrBbLoAzBqTvSgnOy/fUDCiFChThtKimu+sOqLaUCoBFd05KeuPHZsrS/PLa3sKn46mjr/Sm5WX9m1h+xIwbfDrQm42PTbtUwOYcX3ERU+YOo7xAYbmaC/L0aAZFFwfzaHvz5n8y4tzAr2T3786Jffvnt2HnY1nvnY2gnGDTg6HKyun25H15OnN+fP4He9Vz+a3uM7tPsKOdx04+c6hsyumT+u9fPa+H6jzKyu27nscVk54uPJIgPy9ebK8N3pw8fTNm1pWbFq5oenCn8XnPwdNlVe+OnGpBvSd+LTg+3urj6/9bXX1TveXs5jTu9dffPSNLyo+e+qlq1vWrfmurf7Yq28udhU+4V337s7Xygs+WPGs+vLlwilvlVQ8OPGTnz27juJEQfjIvIvj3z6w9uS+Ja9PmNkrHO47+uMBR33D+pmnnvMumrRw49Y9UTe82Nx+6OO/er5dU/hNyYcbn3fNORY/D1746MzCbXs2nGqC85/pUVYt31rwk3h876m8UJ7vCvp16f5dU7OFGY1btAuP5ax6oG9b38T+vfwHP81Ar/URAAA=",
+    token = "v^1.1#i^1#f^0#r^0#I^3#p^1#t^H4sIAAAAAAAAAOVYa2gUVxTO5lW20QQf2BICXUeFatjZO6/s7uiubhJDVmMSs2nU0CJ3Zu4kY3ZnJnPvmg1V2KatSilILFWLFe0DtFTFH1KJ1VoCbaFo2wilUNrSH/1hsUiNbS22QmcmMW5S8ZWFBrp/ljn33HO/77vn3HNnQLbUu2xH444bsz2PFR7Ogmyhx8OUAW9pSXV5UWFlSQHIcfAczi7OFg8UXV6BYSppim0Im4aOkS+TSupYdI0RKm3pogGxhkUdphAWiSwmYuuaRJYGomkZxJCNJOWL10coTmXZUI2icDIjsDWSZFv12zHbjQglSQrgpaDEAFkOsWHGHsc4jeI6JlAnEYoFLO8HrJ/l2pmQyAsiD+hgiO2kfB3Iwpqh2y40oKIuXNGda+VgvTdUiDGyiB2EisZjDYmWWLx+dXP7ikBOrOi4DgkCSRpPfqozFOTrgMk0uvcy2PUWE2lZRhhTgejYCpODirHbYB4Bviu1JKuMCgSek7kgh0JcXqRsMKwUJPfG4Vg0xa+6riLSiUb676eorYa0Bclk/KnZDhGv9zl/69MwqakasiLU6trYplhrKxWt67Y0TDToxwhacre/ta3eL0EEBYFlBL/A17C8xHDjy4zFGhd5yjp1hq5ojmTY12yQWmRjRlOV4XKUsZ1a9BYrphIHT44fC24rGAx3Ols6todp0q07u4pStgw+9/H++k/MJsTSpDRBExGmDrgCRShomppCTR10M3E8eTI4QnUTYoqBQF9fH93H0YbVFWABYAIb1zUl5G6UgpTr69S646/df4Jfc6nIyJ6JNZH0mzaWjJ2pNgC9i4ryoXA4DMZ1nwwrOtX6L0MO58DkeshXfQQhCMlAgILMh2EwKOSjPqLjKRpwcCAJ9vtT0OpBxExCGfllO8/SKWRpip1UKsuFVORXasKqnw+rql8SlBo/oyIEEJIkORz6/5TJgyZ6AskWInnK9Dxlebh3aybGENWqDaodoXa7l3VWG4nQlrretrY1tS0oYEG+WRbYDb1dkQethbuSr0tqtjLt9vr5EsCp9fyI0GhggpRp0UvIholajaQm98+sDeYspRVapD+BkknbMC2SMdOM5+ukzhO9hzokHo11PvvTf9Kb7soKOwk7s1g587EdAJoa7XQfWjZSAQPa146AU+u2ebOLelq8NfvOOqNY2yTH2GrK2GWTdinTeKtMWwgbacu+Z9Mtzu2r3ehBut3NiGUkk8jqYKZdzalUmkApiWZaWechwTU4w1otEwShIMOF+enxkt1GunmmHUn5O4iLVz7khTow+eU+WuD+mAHPMBjwfFTo8YAVYAmzCCwsLXqmuGhWJdYIojWo0ljr0u13VgvRPajfhJpVOK9g9O3XG+sqV7fsXfZ8e/9XBz4rmJXzbeHwc+DJia8L3iKmLOdTA6i6M1LCVDwxm+UBy3JMiBd40AkW3RktZhYUzz81IPx1cGlt1Q8/Xt3w6Z9HB6t6tnaC2RNOHk9JQfGAp0DqHL14s6zk3bWZ3kPXRy81VJ9+def3v4+81Xw8dWZO+RL5b29mJLNxd8VLRz7Zd/HL6jcPvFe3yXttePnXxz43y4dOPv5xJHGeWtx86rX43Mvf7hR/WVh5Zv/8VdTgCSXe0J2qLN/4imfVhdPbjm+rOLLg/RtzSt8ZGdr+7CH51rzdzTd/unLTS323J2Ce+6bq6VtrF7dere04JWX3LOee+jCxa822F07uOnhm4bDZVj0QiQ1eTJ6tX/JF7QnhZ++xpuj2+kObmzYouE/K/mbNu7L+156Xz41cOaEPnx8KDf3xYlVZ+VJw7foH5Sx9qfHGXN+Rwa4FJZTKVKyEb+y/sFfIHA2cBefWlY6O7eU/Fm54oPURAAA="
+    url = "https://api.ebay.com/buy/browse/v1/item_summary/search?q="+item+"&limit="+str(count)+"&filter=buyingOptions:{FIXED_PRICE}&sort=price"
+    headers = {"Authorization":"Bearer "+token,
                 "X-EBAY-C-MARKETPLACE-ID":"EBAY_US",
-                "X-EBAY-C-ENDUSERCTX":"affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>"}
+                "X-EBAY-C-ENDUSERCTX":"affiliateCampaignId=<ePNCampaignId>,affiliateReferenceId=<referenceId>",
+                "X-EBAY-C-ENDUSERCTX": "contextualLocation=country%3DUS%2Czip%3D19406"}
     apiReq = requests.get(url,headers=headers)
     parseddoc = apiReq.json()
     print(parseddoc)
-    # prices = pd.Series(index = np.arange(count))
-    # ids = [None]*count
-    # i = 0
-    # for item in (parseddoc["itemSummaries"]):
-    #     title = item["title"]
-    #     ids[i]=title
-    #     # condition = item["condition"][0]["conditionDisplayName"][0]
-    #     price = item["price"]["value"]
-    #     prices[i] = price
-    #     i = i + 1
-    # ret = pd.Series(index = ids, data = prices.values)
+    prices = pd.Series(index = np.arange(count))
+    ids = [None]*count
+    i = 0
+    for item in (parseddoc["itemSummaries"]):
+        title = item["title"]
+        ids[i]=title
+        # condition = item["condition"][0]["conditionDisplayName"][0]
+        print(item)
+        price = item["price"]["value"]
+        prices[i] = float(price)
+        i = i + 1
+    ret = pd.Series(index = ids, data = prices.values)
+    print(ret)
     # return ret
 
 if __name__ == "__main__":
