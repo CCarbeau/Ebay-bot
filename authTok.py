4053e8f1fdac0b0d0513fb63b4d469ca3bf35a1b
Christian Carbeau <ctc53@duke.edu>
2024-03-15 10:11:41 -0400

    fixed bug

--- authTok.py
+++ authTok.py
@@ -3,8 +3,6 @@ import requests
 import base64 
 
 def getToken():
-    credential_file = open("config.json","r")
-    creds = json.load(credential_file)
     production_login_endpoint = "https://api.ebay.com/identity/v1/oauth2/token"
 
     ci_cs = "Christia-search-PRD-baea55215-54624b13:PRD-aea55215cf88-b713-4a4d-9f3d-1968"
@@ -15,7 +13,7 @@ def getToken():
     'Content-Type': 'application/x-www-form-urlencoded',
     'Authorization': 'Basic '+auth,
     }
-    scopes = "https://api.ebay.com/oauth/api_scope"
+    
     data = {
         "grant_type":"client_credentials",
         "scope": "https://api.ebay.com/oauth/api_scope"
