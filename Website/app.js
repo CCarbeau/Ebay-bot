b3780de50b55574aad63798ed2f013fedeb891cd
Christian Carbeau <ctc53@duke.edu>
2024-04-01 23:09:57 -0400

    added dropdown

--- Website/app.js
+++ Website/app.js
@@ -1,4 +1,8 @@
-function alertButton(){
-    alert("Hello World!")
-}
-
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("myDropdown");
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+}  
