a846af76338fc3cf9e26d419415596d78f13dce9
Christian Carbeau <ctc53@duke.edu>
2024-04-03 00:43:38 -0400

    added animation on home page

--- Website/app.js
+++ Website/app.js
@@ -6,3 +6,36 @@ function toggleDropdown() {
       dropdownContent.style.display = "block";
     }
 }  
+
+window.addEventListener('scroll', function() {
+  // Calculate the scroll position
+  let scrollPosition = window.scrollY;
+  
+  // Get the image container
+  let container = document.querySelector('.image-container');
+  
+  // Get all the images within the container
+  let images = container.querySelectorAll('img');
+  
+  // Loop through each image
+  images.forEach(function(image,index) {
+      // Calculate the opacity based on the scroll position and image index
+      let opacity = Math.max(0, Math.min(1, (scrollPosition - (index * 300)) / 300));
+      
+      // Apply the opacity to the image
+      image.style.opacity = opacity;
+
+
+  });
+
+  let elements = container.querySelectorAll('.animate');
+  
+  // Loop through each element
+  elements.forEach(function(element, index) {
+      // Calculate the opacity based on the scroll position and element index
+      let opacity = Math.max(0, Math.min(1, (scrollPosition - (index * 150)) / 150));
+      
+      // Apply the opacity to the element
+      element.style.opacity = opacity;
+  });
+});
